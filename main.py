c05c6202185a0f561375005a2ae0e975d3427273
Filipe <filipe.ramos92@hotmail.com>
2026-06-28 20:26:26 -0300

    Primeira versão funcional

--- main.py
+++ main.py
@@ -5,22 +5,40 @@ arquivo_orignal = 'base-analise.xlsx';
 
 arquivo_novo = 'arquivo_final.xlsx';
 
+aloc = {}
+
 config_tipos = {
     "Cód. Porto": str,
 }
 
+abas_criadas = []
+
 df_alocacao = pd.read_excel(arquivo_orignal, sheet_name='Alocação', header=0, index_col=0, dtype=config_tipos)
+
+df_base = pd.read_excel(arquivo_orignal, sheet_name='Base', header=0, index_col=0, dtype=config_tipos)
 
 codigos_porto = df_alocacao['Cód. Porto'].dropna().unique()
 
 wb = openpyxl.load_workbook(arquivo_orignal, data_only=False)
 
-for porto in codigos_porto:
-    nome_aba = porto[:10]
+for index, data_aloc in df_alocacao.iterrows():
+    aloc[data_aloc['Cód. Embarcação']] = data_aloc['Cód. Porto']
+
+for index, data_aloc in df_alocacao.iterrows():
+    nome_aba = data_aloc['Cód. Porto'][:10]
 
     if nome_aba not in wb.sheetnames:
         wb.create_sheet(title=nome_aba)
         print(f"Aba '{nome_aba}' criada com sucesso!")
+        abas_criadas.append(nome_aba)
+
+for index, data_base in df_base.iterrows():
+
+    if data_base['Cód. Porto'] != aloc[data_base['Cód. Embarcação']]:
+        if data_base['Cód. Porto'] in abas_criadas:
+            aba = wb[aloc[data_base['Cód. Embarcação']]]
+            aba.append(data_base.tolist())
+            print(f"Dados da linha {index} adicionados à aba '{nome_aba}'.")
 
 wb.save(arquivo_novo)
 
